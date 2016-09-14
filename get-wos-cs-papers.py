da801f045e890661a1c8dbd8c37a36c4feeafc93
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-09-13 22:12:21 -0400

    get-wos-cs-papers.py

---
+++ get-wos-cs-papers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import argparse
+import ConfigParser
+import MySQLdb
+import pickle
+
+
+def wos_cs_papers(cursor):
+    cursor.execute("""
+        SELECT DISTINCT uid FROM papers, subjects
+        WHERE papers.uid = subjects.paperid AND subjects.subject
+        LIKE 'computer%';""")
+
+    result = cursor.fetchall()
+    return [d[0] for d in result]
+
+
+def connect_db(config, target):
+    host = config.get(target, 'host')
+    username = config.get('wos', 'username')
+    password = config.get('wos', 'password')
+    database = config.get('wos', 'database')
+
+    return MySQLdb.connect(host=host, user=username, passwd=password, db=database)
+
+
+def main(args, config):
+    try:
+        db = connect_db(config, 'wos')
+        cursor = db.cursor()
+        cs_papers = wos_cs_papers(cursor)
+        with open(args.output, 'wb') as f:
+            pickle.dump(cs_papers, f)
+    finally:
+        if db:
+            db.close()
+
+
+if __name__ == '__main__':
+    config = ConfigParser.ConfigParser()
+    config.read('config.ini')
+
+    parser = argparse.ArgumentParser(description='Get CS papers in Web of Science.')
+    parser.add_argument('-o', '--output', required=True, help='Output pickle file')
+
+    args = parser.parse_args()
+    main(args, config)
