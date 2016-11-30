6f0e7b2b548a06b55657980d5f811dabdc8e8e2c
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-11-29 22:55:28 -0500

    sample-with-cornelia-method.py

---
+++ sample-with-cornelia-method.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import argparse
+import configparser
+import csv
+import MySQLdb
+import os
+import random
+import sys
+
+import dataclean.wos as wos
+import dataclean.citeseerx as csx
+
+
+def connect_db(config, target):
+    host = config.get(target, 'host')
+    username = config.get(target, 'username')
+    password = config.get(target, 'password')
+    database = config.get(target, 'database')
+
+    return MySQLdb.connect(host=host, user=username, passwd=password, db=database)
+
+
+def match(csvwriter, wos_paperid):
+    wos_paper = wos.WosPaper.get_paper_by_id(wos_cursor, wos_paperid)
+
+    # ignore papers without title
+    if not wos_paper.title:
+        return False
+
+    print(wos_paper.paper_id)
+    csvwriter.writerow(['WoS uid', wos_paper.paper_id])
+    csvwriter.writerow(['Title', wos_paper.title])
+
+    # match title
+    cg_clusters = csx.CgCluster.find_clusters_by_title_on_solr_imprecise(solr_url, wos_paper.title)
+    if not cg_clusters:
+        return True
+
+    # ignore clusters without corresponding DOIs
+    for cg_cluster in cg_clusters:
+        dois = cg_cluster.get_dois(cg_cursor)
+        if not dois:
+            continue
+        authors = cg_cluster.get_authors(cg_cursor)
+        csvwriter.writerow(['Cluster ID', cg_cluster.paper_id])
+        csvwriter.writerow(['Title', cg_cluster.title])
+        csvwriter.writerow(['Authors'] + authors)
+        csvwriter.writerow(['DOIs'] + dois)
+    return True
+
+
+def sampling(csvwriter, wos_paperids, nsamples):
+    random.shuffle(wos_paperids)
+    count = 0
+    for wos_paperid in wos_paperids:
+        if match(csvwriter, wos_paperid):
+            count += 1
+            csvwriter.writerow(['#####'])
+
+        if count >= nsamples:
+            break
+
+
+def main(args, config):
+    global wos_cursor
+    global cg_cursor
+    global solr_url
+
+    solr_url = config.get('solr', 'url')
+
+    if args.infile:
+        inf = open(args.infile)
+    else:
+        inf = sys.stdin
+
+    print('loading input...')
+    wos_paperids = []
+    csvreader = csv.reader(inf)
+    for row in csvreader:
+        wos_paperid = row[0]
+        wos_paperids.append(wos_paperid)
+
+    if args.outfile:
+        outf = open(args.outfile, 'w')
+    else:
+        outf = open(os.devnull, 'w')
+    csvwriter = csv.writer(outf)
+
+    try:
+        wosdb = None
+        cgdb = None
+        wosdb = connect_db(config, 'wos')
+        cgdb = connect_db(config, 'citegraph')
+        wos_cursor = wosdb.cursor()
+        cg_cursor = cgdb.cursor()
+
+        print('sampling...')
+        sampling(csvwriter, wos_paperids, args.nsamples)
+
+    finally:
+        if wosdb:
+            wosdb.close()
+        if cgdb:
+            cgdb.close()
+
+
+if __name__ == '__main__':
+    wos_cursor = None
+    cg_cursor = None
+    solr_url = None
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    parser = argparse.ArgumentParser(description='Sample matched papers in Web of Science and papers in CiteSeerX that have DOIs.')
+    parser.add_argument('-i', '--infile', help='input CSV file of CS papers')
+    parser.add_argument('-o', '--outfile', help='output CSV file of sample results')
+    parser.add_argument('-n', '--nsamples', type=int, default=1000, help='number of samples')
+
+    args = parser.parse_args()
+    main(args, config)
