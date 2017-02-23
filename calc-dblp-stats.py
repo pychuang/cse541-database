00ff7310615eb927b46ad3513961abd63a3aab28
Po-Yu Chuang <ratbert.chuang@gmail.com>
2017-02-22 23:03:54 -0500

    calc-dblp-stats.py: iterative parse XML

--- calc-dblp-stats.py
+++ calc-dblp-stats.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 
 from collections import defaultdict
 from lxml import etree
 
 
 def main(args):
-    with open(args.infile) as f:
-        print("loading %s ..." % args.infile)
-        parser = etree.XMLParser(load_dtd=True)
-        tree = etree.parse(f, parser)
-
-    root = tree.getroot()
-
     stats = defaultdict(int)
-    for child in root:
-        stats[child.tag] += 1
+    with open(args.infile, 'rb') as f:
+        print("loading %s ..." % args.infile, file=sys.stderr)
+        xmldoc = etree.iterparse(f, load_dtd=True, events=('start', 'end'))
+        _, root = next(xmldoc)
+
+        count = 0
+        current_tag = None
+        for event, elem in xmldoc:
+            if event == 'end' and elem.tag == current_tag:
+                #print(elem.findtext('title'))
+                current_tag = None
+                elem.clear()
+            if event == 'start' and current_tag is None:
+                current_tag = elem.tag
+                stats[elem.tag] += 1
+                count += 1
+
+                print(count, file=sys.stderr, end='\r')
+                sys.stderr.flush()
 
     print(stats)
 
