a5160159b1f7f467534e226af672c45f6303b1af
Po-Yu Chuang <ratbert.chuang@gmail.com>
2017-02-22 23:42:55 -0500

    calc-dblp-stats.py -> plot-dblp-stats.py

--- plot-dblp-stats.py
+++ plot-dblp-stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import matplotlib.pyplot as plt
+import operator
 import sys
 
 from collections import defaultdict
@@ -30,6 +32,26 @@ def main(args):
                 sys.stderr.flush()
 
     print(stats)
+
+    sorted_stats = sorted(stats.items(), reverse=True, key=operator.itemgetter(1))
+    print(sorted_stats)
+
+    left_x = range(len(stats))
+    heights = []
+    bar_labels = []
+
+    for k, v in sorted_stats:
+        heights.append(v)
+        bar_labels.append(k)
+
+    plt.title('Bibliographic records in DBLP')
+    plt.bar(left_x, heights, align='center')
+    plt.xticks(left_x, bar_labels, rotation=60)
+    plt.xlabel('types')
+    plt.ylabel('count')
+    plt.tight_layout()
+    plt.savefig('dblp-record-types.png')
+    plt.show()
 
 
 if __name__ == '__main__':
