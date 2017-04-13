c1c959f16fdb56dcb47e9cbfe9ab0b74a00f7c6a
Po-Yu Chuang <ratbert.chuang@gmail.com>
2017-04-12 23:45:35 -0400

    algorithm rename

--- batch-plot-title-OR-citation-matching.sh
+++ batch-plot-title-OR-citation-matching.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./batch-plot.py -s summary-title-OR-citation-matching.csv -m jaccard -t "Title OR Citation Matching (Jaccard of citations)" -b summary_plots -p title-OR-citation-matching-jaccard
-./plot-summary-xy.py -i summary-title-OR-citation-matching.csv -m jaccard -t "Precision v.s. Recall for Title OR Citation Matching (Jaccard of citations)" -x Recall -y Precision -c F1 -o summary_plots/title-OR-citation-matching-jaccard/title-OR-citation-matching-jaccard-xy-precision-recall.png --no-show
+./batch-plot.py -s summary-title-OR-citation-matching.csv -m jaccard -t "Joint (Jaccard of citations)" -b summary_plots -p title-OR-citation-matching-jaccard
+./plot-summary-xy.py -i summary-title-OR-citation-matching.csv -m jaccard -t "Precision v.s. Recall for Joint (Jaccard of citations)" -x Recall -y Precision -c F1 -o summary_plots/title-OR-citation-matching-jaccard/title-OR-citation-matching-jaccard-xy-precision-recall.png --no-show
 
-./batch-plot.py -s summary-title-OR-citation-matching.csv -m nnmr -t "Title OR Citation Matching (non-NULL citation matched ratio)" -b summary_plots -p title-OR-citation-matching-nnmr
-./plot-summary-xy.py -i summary-title-OR-citation-matching.csv -m nnmr -t "Precision v.s. Recall for Title OR Citation Matching (Jaccard of citations)" -x Recall -y Precision -c F1 -o summary_plots/title-OR-citation-matching-nnmr/title-OR-citation-matching-nnmr-xy-precision-recall.png --no-show
+./batch-plot.py -s summary-title-OR-citation-matching.csv -m nnmr -t "Joint (non-NULL citation matched ratio)" -b summary_plots -p title-OR-citation-matching-nnmr
+./plot-summary-xy.py -i summary-title-OR-citation-matching.csv -m nnmr -t "Precision v.s. Recall for Joint (Jaccard of citations)" -x Recall -y Precision -c F1 -o summary_plots/title-OR-citation-matching-nnmr/title-OR-citation-matching-nnmr-xy-precision-recall.png --no-show
 
-./batch-plot.py -s summary-title-OR-citation-matching.csv -m mr -t "Title OR Citation Matching (citation matched ratio)" -b summary_plots -p title-OR-citation-matching-mr
-./plot-summary-xy.py -i summary-title-OR-citation-matching.csv -m mr -t "Precision v.s. Recall for Title OR Citation Matching (Jaccard of citations)" -x Recall -y Precision -c F1 -o summary_plots/title-OR-citation-matching-mr/title-OR-citation-matching-mr-xy-precision-recall.png --no-show
+./batch-plot.py -s summary-title-OR-citation-matching.csv -m mr -t "Joint (citation matched ratio)" -b summary_plots -p title-OR-citation-matching-mr
+./plot-summary-xy.py -i summary-title-OR-citation-matching.csv -m mr -t "Precision v.s. Recall for Joint (Jaccard of citations)" -x Recall -y Precision -c F1 -o summary_plots/title-OR-citation-matching-mr/title-OR-citation-matching-mr-xy-precision-recall.png --no-show
--- batch-plot-title-matching.sh
+++ batch-plot-title-matching.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir -p summary_plots/title-matching
-./plot-summary-xy-line.py -i summary-title-matching.csv -t "Precision of Title Matching" -x Threshold -y Precision -o summary_plots/title-matching/title-matching-precision.png --no-show
-./plot-summary-xy-line.py -i summary-title-matching.csv -t "Recall of Title Matching" -x Threshold -y Recall -o summary_plots/title-matching/title-matching-recall.png --no-show
-./plot-summary-xy-line.py -i summary-title-matching.csv -t "F1 of Title Matching" -x Threshold -y F1 -o summary_plots/title-matching/title-matching-f1.png --no-show
-./plot-summary-xy-line.py -i summary-title-matching.csv -t "Precision v.s. Recall for Title Matching" -x Recall -y Precision -o summary_plots/title-matching/title-matching-xy-precision-recall.png --no-show
+./plot-summary-xy-line.py -i summary-title-matching.csv -t "Precision of Match-By-Title" -x Threshold -y Precision -o summary_plots/title-matching/title-matching-precision.png --no-show
+./plot-summary-xy-line.py -i summary-title-matching.csv -t "Recall of Match-By-Title" -x Threshold -y Recall -o summary_plots/title-matching/title-matching-recall.png --no-show
+./plot-summary-xy-line.py -i summary-title-matching.csv -t "F1 of Match-By-Title" -x Threshold -y F1 -o summary_plots/title-matching/title-matching-f1.png --no-show
+./plot-summary-xy-line.py -i summary-title-matching.csv -t "Precision v.s. Recall for Match-By-Title" -x Recall -y Precision -o summary_plots/title-matching/title-matching-xy-precision-recall.png --no-show
 
